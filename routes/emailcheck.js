d14f56248664a7ee29076868383cba3301e9814d
songchip <scv487@naver.com>
2019-10-07 15:45:45 +0900

    scv

---
+++ routes/emailcheck.js
@@ -0,0 +1,40 @@
+
+var express = require('express');
+const models = require('../models');
+var router = express.Router();
+
+router.get('/', function (req, res, next) {
+    let email = req.query.email;
+    console.log(email);
+
+
+    models.User.update({
+        auth: "1",
+    }, {
+        where: { email: email }
+    }).then(result => {
+        console.log(result, "권한 추가 완료");
+        res.redirect('../login');
+    }).catch(err => {
+        console.log("에러!!!!!!!!!!!!!!!!!!");
+        console.log(err);
+    });
+
+
+    // var sql = "UPDATE users SET auth = '1' WHERE email = ?";
+
+    // dbconn.query(sql, email, function(err, results){
+    //     if(err){
+    //         console.log(err);
+    //         res.render('incorrect_email');
+    //         return;
+    //     }else{
+    //         console.log(results);
+    //     }
+    // });
+
+    // token이 일치하면 테이블에서 email을 찾아 회원가입 승인 로직 구현
+})
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,17 +1,32 @@
+var path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser')
 const app = express();
+var sequelize = require('./models/index').sequelize;
+
 app.use(bodyParser.json());
 
 
-app.use('/users',require('./routes/users'));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+
+app.use('/login', require('./routes/login'));
+app.use('/users', require('./routes/users'));
+app.use('/register', require('./routes/register'));
+app.use('/emailcheck', require('./routes/emailcheck'));
+
+
 
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 
- 
+  // require('./models').sequelize.sync({force:flase})
+  // .then(()=>{
+  //   console.log('Databases sync');
+  // });
 });
 
-
+sequelize.sync();
 
