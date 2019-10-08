d3508b3cf622ab5e2c00973719cc0fbd6b216e10
dongwankim <zkdlwu94@naver.com>
2019-10-08 16:04:25 +0900

    dongwan 2019-10-07 16:00

--- migrations/20191001081056-create-tradeboard.js
+++ migrations/20191001081056-create-tradeboard.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('tradeboards', {
+    return queryInterface.createTable('TBoard', {
       id: {
         allowNull: false,
         autoIncrement: true,
