101fc18e777dfc6097fd9672e0ec781102effd00
marf3927 <marf3927@gmail.com>
2019-11-05 12:59:54 +0900

    cmt

--- server.js
+++ server.js
@@ -45,15 +45,11 @@ app.io = require('socket.io')(server, {
 var clients = [];
 
 app.io.on('connection', (socket) => {
-<<<<<<< HEAD
+
   const req = socket.request;
   //console.log('SOCKET.REQUEST = ',req);
-  
-  
 
-=======
   console.log('connect!')
->>>>>>> d5fb036f827c15b2a10eb8e5192acfc928f34d6a
   socket.on('login', (data) => {
     var clientInfo = new Object();
     clientInfo.uid = data.uid;
@@ -68,6 +64,7 @@ app.io.on('connection', (socket) => {
   });
 
   socket.on('alarm', (msg) => {
+    console.log('alarm eeeee')
     socket.emit('alarm', msg);
   });
 
