58df6da944e816595f752e4e674ccb8101b2e926
dizzyone <zkdlwu94@naver.com>
2019-11-08 12:33:45 +0900

    pull commit

--- routes/mypage.js
+++ routes/mypage.js
@@ -28,6 +28,7 @@ router.get('/user', function (req, res) {
         res.status(401).send(e)
     })
 });
+
 router.get('/getbalance',function(req,res){
     const addr = req.param('address')
     console.log('addr = ? get aba =',addr)
@@ -67,10 +68,7 @@ router.get('/wallet', function (req, res) {
             UserId: req.param('id')
         }
     }).then((wallet) => {
-<<<<<<< HEAD
 
-=======
->>>>>>> ec6003a7baca605dfb537197d9a7860450ffe960
         if (!wallet) {
             res.status(404)('유저 지갑이 없습니다.');
         } else {
