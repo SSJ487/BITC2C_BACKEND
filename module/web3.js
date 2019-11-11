01bb6712b8907ac283c3af92c850fdb20d009c49
dizzyone <zkdlwu94@naver.com>
2019-11-11 15:23:56 +0900

    abi 파일

--- module/web3.js
+++ module/web3.js
@@ -2,9 +2,14 @@ const Web3 = require('web3');
 var contract = require("truffle-contract");
 const util = require('util');
 const assert = require('assert')
+const fs = require('fs');
+
+
 
 var web3Provider = new Web3.providers.HttpProvider('http://192.168.1.179:22000');
 var web3 = new Web3(web3Provider);
+
+
 
 function createwallet(password){
     return web3.eth.personal.newAccount(password)
@@ -15,4 +20,8 @@ function getbalance(address){
     return web3.eth.getBalance(address)
 }
 
-module.exports ={createwallet,getbalance}
+function callcontract(){
+    const tt_contract_json = fs.readFileSync('C:/Users/user/Desktop/Atoken/build/contracts/Atoken.json');
+
+}
+module.exports ={createwallet,getbalance,callcontract}
--- routes/web3.js
+++ routes/web3.js
@@ -1,18 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const Web3 = require('web3');
-var contract = require("truffle-contract");
-const util = require('util');
-const assert = require('assert')
 
-var web3Provider = new Web3.providers.HttpProvider('http://192.168.1.179:22000');
-var web3 = new Web3(web3Provider);
+const web3 = require('../module/web3');
 
-router.post('/createwallet',(req,res)=>{
-    web3.eth.personal.newAccount(req.body.password).then((response)=>{
-        res.json(response);
-    })
- 
+
+router.get('/test',(req,res)=>{
+    console.log("e");
+   res.json(web3.callcontract())
 
 
 })
